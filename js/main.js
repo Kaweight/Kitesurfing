ac9fa5ec0b1fe7a5288835be63b92877ffc6eca8
Kamil Mazelanik <kamil.mazelanik.ext@eworkgroup.com>
2023-02-23 17:44:23 +0100

    Add footer and function for burger btn to change color

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const burgerButton = document.querySelector(".burger-btn");
 const nav = document.querySelector(".nav");
 const allNavItems = document.querySelectorAll(".nav__item");
+const navButton = document.querySelector(".burger-btn__bars");
+const sections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 const handleNav = () => {
   nav.classList.toggle("nav--active");
@@ -10,6 +13,8 @@ const handleNav = () => {
       nav.classList.remove("nav--active");
     });
   });
+
+  navButton.classList.remove("black-bars-color");
 
   handleNavItemsAnimation();
 };
@@ -24,4 +29,29 @@ const handleNavItemsAnimation = () => {
   });
 };
 
+const handleNavColor = () => {
+  const currentSection = window.scrollY;
+
+  sections.forEach((section) => {
+    if (
+      section.classList.contains("white-section") &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navButton.classList.add("black-bars-color");
+    } else if (
+      !section.classList.contains("white-section") &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navButton.classList.remove("black-bars-color");
+    }
+  });
+};
+
+const handleCurrentYear = () => {
+  const year = new Date().getFullYear();
+  footerYear.innerText = year;
+};
+
+handleCurrentYear();
 burgerButton.addEventListener("click", handleNav);
+window.addEventListener("scroll", handleNavColor);
